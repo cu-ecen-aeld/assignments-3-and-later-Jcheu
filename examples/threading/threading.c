a4b2ddc1f7d5ca73077c40d3c3d753becbda2f7f
jcheu <6410599+Jcheu@users.noreply.github.com>
2023-11-15 23:51:06 -0700

    Assignment4 Part1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -11,9 +11,34 @@
 void* threadfunc(void* thread_param)
 {
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
+    // wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    ThreadData* thread_func_args = (ThreadData*) thread_param;
+    
+    //Wait to obtain the lock
+    usleep(thread_func_args->wait_to_obtain_ms);
+    int rc = pthread_mutex_lock(thread_func_args->lock);
+    
+    //if error, return the data for joiner to free memory
+    if(rc != 0)
+    {
+    	ERROR_LOG("Failed to get lock, error=%d", rc);
+    	return thread_param;
+    }
+    
+    //Got the lock, wait til release
+    usleep(thread_func_args->wait_to_release_ms);
+    rc = pthread_mutex_unlock(thread_func_args->lock);
+    
+    //if error, return the data for joiner to free memory
+    if(rc != 0)
+    {
+    	ERROR_LOG("Failed to unlock, error=%d", rc);
+    	return thread_param;
+    }
+    
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -21,13 +46,28 @@ void* threadfunc(void* thread_param)
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
+     * allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
      * using threadfunc() as entry point.
      *
      * return true if successful.
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    
+    ThreadData* threadData = malloc(sizeof(ThreadData));
+    threadData->lock = mutex;
+    threadData->wait_to_obtain_ms = wait_to_obtain_ms;
+    threadData->wait_to_release_ms = wait_to_release_ms;
+    threadData->thread_complete_success = false;
+     
+    int rc = pthread_create(thread, NULL, threadfunc, (void*) threadData);
+    
+    //If couldn't make thread, print error, free memory
+    if(rc != 0)
+    {
+    	ERROR_LOG("Failed to create thread error=%d", rc);
+    	return false;
+    }
+    return true;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -7,20 +7,23 @@
  * It should be returned by your thread so it can be freed by
  * the joiner thread.
  */
-struct thread_data{
+typedef struct thread_data{
     /*
-     * TODO: add other values your thread will need to manage
+     * add other values your thread will need to manage
      * into this structure, use this structure to communicate
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
+    pthread_mutex_t *lock;
 
     /**
      * Set to true if the thread completed with success, false
      * if an error occurred.
      */
     bool thread_complete_success;
-};
+} ThreadData;
 
 
 /**
